ba713076c368786411c2565d041c101eeb332f57
carito-ro <kro.robles@gmail.com>
2020-09-10 18:34:27 -0300

    Enlace a la BDPrueba para mostrar memes

--- app/src/components/main.js
+++ app/src/components/main.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './main.scss';
 import ListCategory from './listCategory';
+import datos from '../basePrueba/base';
 import ListMeme from './listMeme';
 
 import { useParams } from "react-router-dom";
 function Main() {
     let { name } = useParams();
-
     return (
         <div className="container-fluid">
             <div className="row vh-100">
                 <div className="col-md-4 p-3">
-                    <ListCategory></ListCategory>
+                    <ListCategory categoties={datos.categories}></ListCategory>
                 </div>
                 <div className="col-md-8 p-3 ">
                     Parametro name es: {name}
-                    <ListMeme></ListMeme>
+                    <ListMeme categoryView={name}></ListMeme>
                 </div>
             </div>
         </div>
